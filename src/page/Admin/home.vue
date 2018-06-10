1a74890ceee8afe9a15995fbb8ad7d5c5024542a
libp <924701358@qq.com>
2018-06-10 09:06:50 +0800

    优化管理员首页显示效果

--- src/page/Admin/home.vue
+++ src/page/Admin/home.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
     <head-top></head-top>
-    <section class="data_section">
-      <header class="section_title">数据统计</header>
-      <el-row :gutter="20" style="margin-bottom: 10px;">
-        <el-col :span="4"><div class="data_list today_head"><span class="data_num head">精品文章：</span></div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num">{{articleCount}}</span> 篇</div></el-col>
-        <el-col :span="4"><div class="data_list">已推荐<span class="data_num">{{recommendCount}} 篇</span></div></el-col>
+    <div class="dashboard-editor-container">
+      <el-row class="panel-group" :gutter="20">
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class='card-panel'>
+            <div class="card-panel-icon-wrapper icon-people">
+              <img src="../../assets/count.svg">
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">文章</div>
+              <span class="card-panel-num">{{articleCount}} 篇</span>
+            </div>
+          </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-message">
+              <img src="../../assets/nice.svg">
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">已推荐</div>
+              <span class="card-panel-num">{{recommendCount}} 篇</span>
+            </div>
+          </div>
+        </el-col>
       </el-row>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -36,21 +54,64 @@ export default {
       function selectRecommendArticleCount () {
         return axios.get('/v1/article/selectRecommendArticleCount')
       }
-      axios.all([selectArticleCount(), selectRecommendArticleCount()]).then(
-        axios.spread(function (acct, perms) {
-          console.log('User', acct.data)
-          console.log('Repositories', perms.data)
-          this.articleCount = acct.data
-          // if (acct.status === 200 && perms.status === 200) {
-          //   this.articleCount = acct.data
-          //   this.recommendCount = perms.data
-          // }
-        })
-      )
+      axios.all([selectArticleCount(), selectRecommendArticleCount()])
+        .then(axios.spread((acct, perms) => {
+          if (acct.status === 200) {
+            this.articleCount = acct.data
+            this.recommendCount = perms.data
+          }
+        }))
     }
   }
 }
 </script>
 
 <style lang="less">
+  .dashboard-editor-container {
+    padding: 32px;
+    background-color: #fff;
+    .chart-wrapper {
+      background: #fff;
+      padding: 16px 16px 0;
+      margin-bottom: 32px;
+    }
+  }
+  .panel-group {
+    margin-top: 18px;
+    .card-panel-col {
+      margin-bottom: 32px;
+    }
+    .card-panel {
+      height: 124px;
+      font-size: 12px;
+      position: relative;
+      overflow: hidden;
+      color: #666;
+      background: #fff;
+      box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
+      border-color: rgba(0, 0, 0, .05);
+      .card-panel-icon-wrapper {
+        float: left;
+        margin: 14px 0 0 14px;
+        padding: 16px;
+        transition: all 0.38s ease-out;
+        border-radius: 6px;
+      }
+      .card-panel-description {
+        float: right;
+        font-weight: bold;
+        margin: 26px;
+        margin-left: 0px;
+        .card-panel-text {
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 16px;
+          margin-bottom: 12px;
+        }
+        .card-panel-num {
+          font-size: 20px;
+        }
+      }
+    }
+  }
 </style>
