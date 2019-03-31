67fd5aafaade0ce501bc69ff730e5310199fe829
libp <924701358@qq.com>
2019-03-31 11:08:45 +0800

    update nav

--- src/page/programmernav/nav.vue
+++ src/page/programmernav/nav.vue
@@ -1,17 +1,21 @@
-<template xmlns="http://www.w3.org/1999/html">
-  <div class="navMain">
-    <div v-for="(urls, key) in urlMap" :key="urls.id">
-      <h2>{{ key }}</h2>
-      <div v-for="list in urls" class="urlmessages" :key="list.id">
-        <a v-bind:href="list.weburl" target="_blank" style="color: #3a8ee6">
-          <li style="width: 400px;">{{list.webtitle}}</li>
-          <!--<button v-text="btnText" @click="showToggle"></button>-->
-        </a>
-        <li class="details">{{list.webdesc}}</li>
-        <li class="details">{{list.keywords}}</li>
-      </div>
-      <br/>
-    </div>
+<template>
+  <div>
+    <el-container>
+        <el-header>程序员网址导航</el-header>
+        <el-main>
+          <div class="main" >
+            <div v-for="(urls) in urlList" v-bind:key="urls.id">
+              <div class="url_title"><span>&nbsp;&nbsp;{{ urls.title }}</span></div>
+              <ul class="url_list">
+                <li v-for="(item) in urls.lists" v-bind:key="item.id" class="url_item">
+                  <a :href=item.url target="_blank" :title=item.title> {{ item.name }} </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </el-main>
+        <el-footer>Copyright ©2018~2019</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -19,40 +23,31 @@
 import axios from 'axios'
 import { Loading } from 'element-ui'
 export default {
-  name: 'home',
   data () {
     return {
-      urlMap: {},
-      isShow: false,
-      btnText: '查看详情'
+      urlList: []
     }
   },
   created () {
+    this.getUrlList()
   },
   methods: {
-    selectNav () {
-      let loadingInstance = Loading.service({ target: '.content-area', text: '加载中' })
-      axios.get('/v1/nav/selectNav').then((response) => {
+    getUrlList () {
+      let loadingInstance = Loading.service({ fullscreen: true, text: '加载中' })
+      let t = Math.random()
+      let url = 'urllist.json'
+      axios.get('static/' + url + '?t=' + t).then((response) => {
         if (response.status === 200) {
-          console.log(response)
-          this.urlMap = response.data
+          this.urlList = response.data.params
+          console.log(response.data.params)
           loadingInstance.close()
         }
       }).catch(function (err) {
         console.error(err)
       })
-    },
-    showToggle: function () {
-      this.isShow = !this.isShow
-      if (this.isShow) {
-        this.btnText = '隐藏'
-      } else {
-        this.btnText = '查看详情'
-      }
     }
   },
   mounted () {
-    this.selectNav()
   },
   beforeMount () {
   }
@@ -60,16 +55,54 @@ export default {
 </script>
 
 <style scoped>
-  .navMain{
-    margin: 50px 0 0 100px;
+  .el-container{
+    font-size: 14px;
   }
-  .details{
-    display:none;
+  .el-header {
+    line-height: 60px;
+    text-align: center;
+    font-size: 16px;
+    font-weight: bold;
   }
-  .urlmessages:hover .details{
-    display:block ;
+  .el-footer {
+    line-height: 60px;
+    text-align: center;
   }
-  .urlmessages:visited .details{
-    display:block ;
+  .el-main {
+    background-color: #FFFFFF;
+  }
+  .info{
+    margin-bottom: 2rem;
+  }
+  .main{
+    width: 60rem;
+    height: auto;
+    margin: 0 auto;
+    border: 1px solid #F4F4F5;
+    background: #FFFFFF;
+  }
+  .url_title{
+    width: 60rem;
+    height: 2rem;
+    background-color: #F4F4F5;
+  }
+   .url_title span{
+    padding: 7px;
+    float: left;
+    font-weight: bold;
+  }
+  .url_list{
+    width: 60rem;
+    height: auto;
+    margin: 1rem 0;
+  }
+  .url_item{
+      margin-left: 1rem;
+      margin-right: 2rem;
+      /* width: 7rem; */
+      white-space:nowrap;
+      text-overflow:ellipsis;
+      overflow:hidden;
+      display:inline-block;
   }
 </style>
